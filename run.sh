0f405e73a7fa71712e4a8a365a5d6f11036fc1af
Björn Kjeholt <dev@kjeholt.se>
2016-12-21 07:14:41 +0100

    Update run.sh

--- run.sh
+++ run.sh
@@ -5,7 +5,7 @@ DOCKER_CONTAINER_NAME=hic-agent-onewire
 echo "------------------------------------------------------------------------"
 echo "-- Run image:       $DOCKER_IMAGE_NAME:latest "
 
-DOCKER_IMAGE=$(../SupportFiles/DockerSupport/get-latest-image-string.sh $DOCKER_IMAGE_NAME)
+# DOCKER_IMAGE=$(../SupportFiles/DockerSupport/get-latest-image-string.sh $DOCKER_IMAGE_NAME)
 
 echo "------------------------------------------------------------------------"
 echo "-- Remove docker container if it exists"
@@ -16,7 +16,7 @@ docker rm -f $DOCKER_CONTAINER_NAME
 
 echo "------------------------------------------------------------------------"
 echo "-- Start container "
-echo "-- Based on image: $DOCKER_IMAGE "
+echo "-- Based on image: $DOCKER_IMAGE-NAME "
 echo "------------------------------------------------------------------------"
 
 docker run -d \
@@ -25,5 +25,5 @@ docker run -d \
            --privileged \
            --device /dev:/dev \
            --name $DOCKER_CONTAINER_NAME \
-           $DOCKER_IMAGE
+           $DOCKER_IMAGE-NAME
 
