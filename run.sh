ad9d60c665b5fabc11c38240e7a611d706fbf32b
Björn Kjeholt <dev@kjeholt.se>
2016-12-21 07:43:14 +0100

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,11 +1,15 @@
 #!/bin/sh -f
-DOCKER_IMAGE_NAME=bkjeholt/mqtt-agent-onewire-${1}-rpi
+DOCKER_IMAGE_NAME=bkjeholt/mqtt-agent-onewire
 DOCKER_CONTAINER_NAME=hic-agent-onewire
+
+DOCKER_IMAGE_BASE_TAG=${1}
+
+ARCHITECTURE=rpi
 
 echo "------------------------------------------------------------------------"
 echo "-- Run image:       $DOCKER_IMAGE_NAME:latest "
 
-# DOCKER_IMAGE=$(../SupportFiles/DockerSupport/get-latest-image-string.sh $DOCKER_IMAGE_NAME)
+DOCKER_IMAGE=${DOCKER_IMAGE_NAME}:${DOCKER_IMAGE_BASE_TAG}-${ARCHITECTURE}
 
 echo "------------------------------------------------------------------------"
 echo "-- Remove docker container if it exists"
@@ -16,7 +20,7 @@ docker rm -f $DOCKER_CONTAINER_NAME
 
 echo "------------------------------------------------------------------------"
 echo "-- Start container "
-echo "-- Based on image: $DOCKER_IMAGE-NAME "
+echo "-- Based on image: $DOCKER_IMAGE "
 echo "------------------------------------------------------------------------"
 
 docker run -d \
@@ -25,5 +29,7 @@ docker run -d \
            --privileged \
            --device /dev:/dev \
            --name $DOCKER_CONTAINER_NAME \
-           $DOCKER_IMAGE-NAME
+           --env DOCKER_CONTAINER_NAME=${DOCKER_CONTAINER_NAME} \
+           $DOCKER_IMAGE
+
 
